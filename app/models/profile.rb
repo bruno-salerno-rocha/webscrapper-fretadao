f1325b4c6c962836a67e53ef3f8dee8688bd0c3f
Bruno <brunosalernorocha@gmail.com>
2022-02-14 08:52:31 -0300

    Creating callback to shorten Link's url before saving

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,12 @@
 class Profile < ApplicationRecord
     validates :name, presence: true
     validates :github_url, presence: true
+
+    before_save :shorten_github_url
+
+    private
+
+    def shorten_github_url
+        self.github_url = Link.shorten(self.github_url)
+    end
 end
