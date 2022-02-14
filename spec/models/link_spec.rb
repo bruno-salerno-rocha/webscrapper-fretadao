dc2e9731164ad1354b4e778cdb8e5fce014c0f36
Bruno <brunosalernorocha@gmail.com>
2022-02-13 23:02:46 -0300

    Writing test for Link#short

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:link) {
+    Link.create(url: "http://www.foo.com")
+  }
+
+  describe "#short" do
+    it "returns a link with a 7 digit slug" do
+      expect(link.short.split("/").last.length).to be(7)
+    end
+  end
 end
