e2d8bdb87db62c73fc310d5076c41d2c5d4ed282
Bruno <brunosalernorocha@gmail.com>
2022-02-14 08:36:42 -0300

    Creating Link's self.shorten method and it's tests

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,6 +10,14 @@ class Link < ApplicationRecord
     def short
         Rails.application.routes.url_helpers.short_url(slug: self.slug, host: "localhost:3000")
     end
+
+    def self.shorten(url)
+        link = Link.where(url: url).first
+        return link.short if link 
+          
+        link = Link.new(url: url)
+        return link.short if link.save
+    end
   
     def generate_slug
         self.slug = SecureRandom.uuid[0..6] if self.slug.blank?
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
+  let(:url) { "http://www.foo.com" }
   let(:link) {
-    Link.create(url: "http://www.foo.com")
+    Link.create(url: url)
   }
 
   describe "#short" do
     it "returns a link with a 7 digit slug" do
       expect(link.short.split("/").last.length).to be(7)
+    end
+  end
+
+  describe "#self.shorten" do
+    it "creates a new Link for new URLs" do
+      expect {
+        Link.shorten(url)
+      }.to change(Link, :count).by(1)
+    end
+
+    it "creates only one Link for each URL" do
+      expect {
+        5.times { Link.shorten(url) }
+      }.to change(Link, :count).by(1)
     end
   end
 end
