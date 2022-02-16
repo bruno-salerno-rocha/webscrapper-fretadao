7bcf06ee76239def6cd2c6c63e885dff5c6bc1f4
Bruno <brunosalernorocha@gmail.com>
2022-02-16 10:22:15 -0300

    Adding new tests for Profile#scrape_attributes

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:attributes) do
+    {
+      name: "Name",
+      github_url: "http://www.foo.com"
+    }
+  end
+
+  let(:profile) { Profile.create(attributes) }
+
+  describe "#scrape_attributes" do
+
+    let(:scraped_attributes) do
+      {
+        github_user: "User",
+        followers: 10,
+        following: 10,
+        stars: 5,
+        last_year_contributions: 100,
+        profile_picture_url: "/spec/img/logo.png",
+        organization: "Organization",
+        location: "Location"
+      }
+    end
+
+    it "sets the profile with the attributes from the scraper" do
+      allow_any_instance_of(ProfilesScraper).to receive(:get_profile_attributes) { scraped_attributes }
+      profile.scrape_attributes
+      expect(profile.github_user).to eq(scraped_attributes[:github_user])
+      expect(profile.followers).to eq(scraped_attributes[:followers])
+      expect(profile.following).to eq(scraped_attributes[:following])
+      expect(profile.stars).to eq(scraped_attributes[:stars])
+      expect(profile.last_year_contributions).to eq(scraped_attributes[:last_year_contributions])
+      expect(profile.profile_picture_url).to eq(scraped_attributes[:profile_picture_url])
+      expect(profile.organization).to eq(scraped_attributes[:organization])
+      expect(profile.location).to eq(scraped_attributes[:location])
+    end
+
+    it "maintain the original values of non scraped attributes" do
+      allow_any_instance_of(ProfilesScraper).to receive(:get_profile_attributes) { scraped_attributes }
+      profile.scrape_attributes
+      expect(profile.name).to eq(attributes[:name])
+      expect(profile.github_url).to eq(Link.shorten(attributes[:github_url]))
+    end
+  end
 end
